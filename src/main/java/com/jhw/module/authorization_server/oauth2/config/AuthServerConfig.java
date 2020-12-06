4ce996b6e03bd2e409d026b17c9a801334701d6c
Unknown <unknown@example.com>
2020-12-05 22:28:55 -0500

    AuthServerConfig: -Qualifier a jwt

--- src/main/java/com/jhw/module/authorization_server/oauth2/config/AuthServerConfig.java
+++ src/main/java/com/jhw/module/authorization_server/oauth2/config/AuthServerConfig.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -37,11 +39,9 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     private ClientDetailsService clientDetailsService;
 
     @Autowired
-    @Qualifier("jwt-access-token-store-generator")
     private TokenStore tokenStore;
 
     @Autowired
-    @Qualifier("jwt-access-token-converter-generator")
     private JwtAccessTokenConverter tokenConverter;
 
     /**
