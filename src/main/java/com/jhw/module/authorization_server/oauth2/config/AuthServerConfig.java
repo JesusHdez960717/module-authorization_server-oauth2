37993523b367b74f7521b445ee15eb58f28905ef
Unknown <unknown@example.com>
2020-12-05 22:31:28 -0500

    MISC

--- src/main/java/com/jhw/module/authorization_server/oauth2/config/AuthServerConfig.java
+++ src/main/java/com/jhw/module/authorization_server/oauth2/config/AuthServerConfig.java
@@ -6,12 +6,9 @@
 package com.jhw.module.authorization_server.oauth2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
