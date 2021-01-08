65f092f6f15873a1794ffc36b52ce04288ef614b
Unknown <unknown@example.com>
2021-01-08 16:15:23 -0500

    Refactor a Root101 + Licence

--- src/main/java/com/root101/module/authorization_server/oauth2/service/ResourceOAuth2Implementation.java
+++ src/main/java/com/root101/module/authorization_server/oauth2/service/ResourceOAuth2Implementation.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.root101.module.authorization_server.oauth2.service;
 
 import com.root101.clean.core.domain.services.ResourceHandler;
@@ -8,7 +24,8 @@ import java.net.MalformedURLException;
 
 /**
  *
- * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public class ResourceOAuth2Implementation implements ResourceService {
 
